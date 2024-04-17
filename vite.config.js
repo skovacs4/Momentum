090b5f163e4109964baf5551b8e3433ca5c0f4d7
skovacs4 <kovacsstefy46@gmail.com>
2024-04-17 23:31:35 +0300

    Changed config.

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,14 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()],
+  plugins: [sveltekit()],
+  
+  build: {
+    target: 'esnext', // Specify target for modern browsers
+  },
+  
+  server: {
+    port: 3000, // Specify development server port
+    open: true, // Open browser automatically when server starts
+  },
 });
